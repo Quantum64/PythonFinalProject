4b6e7952b083da4c64ea383fcf0b5d41d80f4482
Dylan <quantum64development@gmail.com>
2019-11-14 14:14:07 -0500

    Add main file

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+ 
+class Game:
+    def start(self):
+        pass
+
+    def render(self):
+        pass
+
+    def tick(self):
+        pass
+ 
+if __name__ == "__main__" :
+    game = Game()
+    game.start()
